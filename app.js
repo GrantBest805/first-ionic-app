4ba6078fa5a2c03028dce12013c3b0b11b39e39f
Grant Best <grantbst@gmail.com>
2019-04-12 01:59:16 -0400

    add app.js with base logic

---
+++ app.js
@@ -0,0 +1,24 @@
+const reasonInput = document.querySelector('#input-reason');
+const amountInput = document.querySelector('#input-amount');
+
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+
+const expenseList = document.querySelector('#expenses-list');
+
+confirmBtn.addEventListener('click', () => {
+  const enteredReason = reasonInput.value;
+  const enteredAmount = amountInput.value;
+
+  if (
+    enteredReason.trim().length <= 0 || 
+    enteredAmount <= 0 || 
+    enteredAmount.trim().length <= 0
+  ) {
+    return;
+  }
+  const newItem = document.createElement('ion-item');
+  newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+  expenseList.appendChild(newItem);
+});
